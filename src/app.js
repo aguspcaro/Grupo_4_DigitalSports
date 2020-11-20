4bd84360894532b847e401790bf777febc101219
davidkopparg <davidkopparg@gmail.com>
2020-11-20 20:40:17 -0300

    add / index.html al app

--- src/app.js
+++ src/app.js
@@ -6,3 +6,15 @@ const app = express();
 app.listen(3030, function() {
     console.log("servidor funcionando")
 });
+
+app.get ("/", function (req,res){
+
+    res.sendFile(__dirname + "/views/index.html")
+    
+    })
+    
+    app.get ("*", function (req,res){
+    
+    res.sendFile(__dirname+ "/public/" + req.url)
+    
+    })
