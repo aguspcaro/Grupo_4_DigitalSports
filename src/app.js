557fc3825edb32dca3e95f2b2b1557924db276d8
davidkopparg <davidkopparg@gmail.com>
2020-11-21 16:19:26 -0300

    agregado links en archivo app.js

--- src/app.js
+++ src/app.js
@@ -7,17 +7,25 @@ app.listen(3030, function() {
     console.log("servidor funcionando")
 });
 
-<<<<<<< HEAD
 app.get ("/", function (req,res){
-
     res.sendFile(__dirname + "/views/index.html")
-    
     })
-    
+app.get ("/", function (req,res){
+    res.sendFile(__dirname + "/views/carrito.html")
+    })    
+app.get ("/", function (req,res){
+    res.sendFile(__dirname + "/views/detail.html")
+    })    
+app.get ("/", function (req,res){
+    res.sendFile(__dirname + "/views/login.html")
+    })
+app.get ("/", function (req,res){
+    res.sendFile(__dirname + "/views/register.html")
+    })
+
+
     app.get ("*", function (req,res){
     
     res.sendFile(__dirname+ "/public/" + req.url)
     
     })
-=======
->>>>>>> b287f6e8cdeb400eacf804282dbb7337fe5a7b66
