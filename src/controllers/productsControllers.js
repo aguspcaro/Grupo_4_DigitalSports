00dc8556c5793720766ecb01db6a735771306343
davidkopparg <davidkopparg@gmail.com>
2020-12-30 19:32:36 -0300

    arreglado productos listado y dar de alta un producto

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -27,7 +27,38 @@ let productsControllers = {
     res.render('admproduct');
 },
   createproduct: function (req, res, next) {
-  res.send('agregaste producto al json');
+  
+        products.push({
+          id: Date.now (),
+          name:req.body.name,
+          description:req.body.coments,
+          image: req.files[0].filename,
+          category:req.body.Categoria,
+          talle:req.body.talle,
+          color:req.body.color,
+          secundario:req.body.secundario,
+          envio:req.body.envio,
+          precio:req.body.precio,
+          promocional:req.body.promocional,
+          stock:req.body.stock,
+          sku:req.body.sku,
+
+        })
+        let producto=JSON.stringify(products);
+          fs.writeFileSync(productsFilePath, producto);
+      
+      res.redirect('/products')
 },
+
+
+
+modificar: function (req, res, next) {
+
+  let codigo=req.params.id;
+  let product=products.find(busca=>busca.id==codigo);
+
+  res.render("producto-modificar",{product});
+
+}
 }
 module.exports = productsControllers;
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
 let productsControllers = require('../controllers/productsControllers');
+const multer = require("multer");
+const path = require('path');
+
+let storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        return cb(null, path.join(".","public","images","products"))
+    } ,
+    
+    filename: function(req,file,cb){
+        return cb(null, Date.now() + path.extname(file.originalname))
+    }
+    
+    })
+    let uploads =multer({storage: storage});
 
 router.get('/', productsControllers.root);
 router.put('/:id', productsControllers.edit);
@@ -9,7 +22,7 @@ router.delete('/:id', productsControllers.delete);
 router.get('/cart', productsControllers.cart);
 router.get('/detail/:id', productsControllers.detail);
 router.get('/create', productsControllers.adm);
-router.post('/create', productsControllers.createproduct);
-
+router.post('/create', uploads.any(),productsControllers.createproduct);
+router.get("/vista/:id", productsControllers.modificar);
 
 module.exports = router;
