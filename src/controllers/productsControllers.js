40dec56f0970df2ae7c94680ee91698c17dbcc65
Alejandro Widomlanski <widomlanski@hotmail.com>
2021-01-02 11:03:14 -0300

    editando el producto

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -3,27 +3,30 @@ const path = require('path');
 
 const productsFilePath = path.join(__dirname, '../data/products.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 
 let productsControllers = {
   root: function (req, res, next) {
-    res.render('products',{products});
-    
+    res.render('products', { products });
   },
+
   edit: function (req, res, next) {
-        res.send("editaste el producto");
+    const filtrado = products.find((producto) => producto.id != req.params.id);
+
+    let producto = JSON.stringify(filtrado);
+
+    fs.writeFileSync(productsFilePath, producto);
+
+    res.redirect('/');
   },
+
   delete: function (req, res, next) {
-    const  filtrado= products.filter((producto)=>
-    producto.id != req.params.id)
+    const filtrado = products.filter((producto) => producto.id != req.params.id);
 
-  let producto=JSON.stringify(filtrado);
-  fs.writeFileSync(productsFilePath, producto);
-  res.redirect('/')
-
-
-},
+    let producto = JSON.stringify(filtrado);
+    fs.writeFileSync(productsFilePath, producto);
+    res.redirect('/');
+  },
   cart: function (req, res, next) {
     res.render('cart');
   },
@@ -32,40 +35,34 @@ let productsControllers = {
   },
   adm: function (req, res, next) {
     res.render('admproduct');
-},
+  },
   createproduct: function (req, res, next) {
-  
-        products.push({
-          id: Date.now (),
-          name:req.body.name,
-          description:req.body.coments,
-          image: req.files[0].filename,
-          category:req.body.Categoria,
-          talle:req.body.talle,
-          color:req.body.color,
-          secundario:req.body.secundario,
-          envio:req.body.envio,
-          precio:req.body.precio,
-          promocional:req.body.promocional,
-          stock:req.body.stock,
-          sku:req.body.sku,
+    products.push({
+      id: Date.now(),
+      name: req.body.name,
+      description: req.body.coments,
+      image: req.files[0].filename,
+      category: req.body.Categoria,
+      talle: req.body.talle,
+      color: req.body.color,
+      secundario: req.body.secundario,
+      envio: req.body.envio,
+      precio: req.body.precio,
+      promocional: req.body.promocional,
+      stock: req.body.stock,
+      sku: req.body.sku,
+    });
+    let producto = JSON.stringify(products);
+    fs.writeFileSync(productsFilePath, producto);
 
-        })
-        let producto=JSON.stringify(products);
-          fs.writeFileSync(productsFilePath, producto);
-      
-      res.redirect('/products')
-},
+    res.redirect('/products');
+  },
 
+  modificar: function (req, res, next) {
+    let codigo = req.params.id;
+    let product = products.find((busca) => busca.id == codigo);
 
-
-modificar: function (req, res, next) {
-
-  let codigo=req.params.id;
-  let product=products.find(busca=>busca.id==codigo);
-
-  res.render("producto-modificar",{product});
-
-}
-}
+    res.render('producto-modificar', { product });
+  },
+};
 module.exports = productsControllers;
