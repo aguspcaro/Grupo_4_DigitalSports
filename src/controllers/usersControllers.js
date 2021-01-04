865a80362618ebf1211ef89eb247d3978b59227d
aguspcaro <aguspcaro@gmail.com>
2021-01-04 19:19:56 -0300

    users

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,9 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const usersFilePath = path.join(__dirname, '../data/users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+
 let usersControllers = {
+  root : function(req, res, next) {
+    res.render("users", {users} );
+  },
   register: function (req, res, next) {
     res.render("register");
   },
   registration: function (req, res, next) {
-    res.send("felicitaciones te has registrado");
+    users.push({
+      id: Date.now(),
+      name: req.body.nameRegister,
+      lastName: req.body.lastNameRegister,
+      email: req.body.emailRegister,
+      password: req.body.passwordRegister,
+      verifyPassword : req.body.confirmPasswordRegister
+    });
+    let usuario = JSON.stringify(users);
+    fs.writeFileSync(usersFilePath, usuario);
+
+    res.redirect('/users');
   },
   login: function (req, res, next) {
     res.render("login");
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,16 +1,24 @@
 var express = require('express');
 var router = express.Router();
-
 let usersControllers = require('../controllers/usersControllers');
+const multer = require('multer');
+const path = require('path');
 
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    return cb(null, path.join('.', 'public', 'images', 'users'));
+  },
+  filename: function (req, file, cb) {
+    return cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+let uploads = multer({ storage: storage });
+
+router.get("/", usersControllers.root);
 router.get('/register', usersControllers.register);
-
-router.post('/register', usersControllers.registration);
-
+router.post('/register', uploads.any(), usersControllers.registration);
 router.get('/login', usersControllers.login);
-
 router.post('/login', usersControllers.checkLogin);
-
 router.post('/suscribe', usersControllers.suscribe); 
 
 module.exports = router;
