c314f3c07f998b6fd346105e98055f30c3630e15
Jorge <jorgejones01@gmail.com>
2021-01-04 21:50:33 -0300

    cambios

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -1,7 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
 let mainControllers = {
   index: function (req, res, next) {
-    res.render('index');
+    
+    
+    res.render('index',{products});
+    
   },
+
+
   search: function (req, res) {
      let palabraBuscada = req.query.homeSearch;
 
