52c054316071a12edb6e83a9f6f35c7ed8127ae6
aguspcaro <aguspcaro@gmail.com>
2021-01-13 09:50:00 -0300

    usuarios validacion

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -4,52 +4,50 @@ const path = require('path');
 const usersFilePath = path.join(__dirname, '../data/users.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+let { check, validationResult, body} = require("express-validator");
+
 
 let usersControllers = {
+
   root : function(req, res, next) {
     res.render("users/users", { users } );
   },
+
   register: function (req, res, next) {
     res.render("users/register");
   },
-  registration: function (req, res, next) {
-    users.push({
-      id: Date.now(),
-      nameRegister: req.body.nameRegister,
-      lastNameRegister: req.body.lastNameRegister,
-      emailRegister: req.body.emailRegister,
-      passwordRegister: req.body.passwordRegister,
-      confirmPasswordRegister : req.body.confirmPasswordRegister
-    });
-    
-    let usuario = JSON.stringify(users);
-    fs.writeFileSync(usersFilePath, usuario);
 
-    res.redirect('users/users');
+  registration: function (req, res, next) {
+    let errors = validationResult(req);
+    console.log(errors)
+    if( errors.isEmpty() ) {
+      users.push({
+        id: Date.now(),
+        name: req.body.name,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword
+      });
+      
+      let usuario = JSON.stringify(users);
+      fs.writeFileSync(usersFilePath, usuario);
+  
+      return res.redirect('/');
+    } else {
+      return res.render("users/register", {errors : errors.errors})
+    }
+    
   },
-  login: function (req, res, next) {
-    res.render("users/login");
-  },
-  checkLogin: function (req, res, next) {
-    res.send("Has ingresado correctamente");
-  },
-  suscribe: function (req, res, next) {
-    res.render("thankYou");
-  },
-  delete : function(req, res) {
-    const filtrar = users.filter((user) => user.id != req.params.id);
-    let user = JSON.stringify(filtrar);
-    fs.writeFileSync(usersFilePath, user);
-    res.redirect("users/users");
-  },
+
   edit : function(req, res) {
     users.forEach(function(user) {
       if (user.id == req.params.id) {
-        user.nameRegister = req.body.nameRegister;
-        user.lastNameRegister = req.body.lastNameRegister;
-        user.emailRegister = req.body.emailRegister;
-        user.passwordRegister = req.body.passwordRegister;
-        user.confirmPasswordRegister = req.body.confirmPasswordRegister;
+        user.name = req.body.name;
+        user.lastName = req.body.lastName;
+        user.email = req.body.email;
+        user.password = req.body.password;
+        user.confirmPassword = req.body.confirmPassword;
       }
     })
     let usuario = JSON.stringify(users);
@@ -57,6 +55,14 @@ let usersControllers = {
 
     res.redirect("users/users")
   },
+
+  delete : function(req, res) {
+    const filtrar = users.filter((user) => user.id != req.params.id);
+    let user = JSON.stringify(filtrar);
+    fs.writeFileSync(usersFilePath, user);
+    res.redirect("users/users");
+  },
+
   modificar : function(req, res) {
     let codigo = req.params.id;
     let user = users.find(function (busca) {
@@ -64,7 +70,22 @@ let usersControllers = {
     });
 
     res.render('users/users', { user });
+  },
+
+  login: function (req, res, next) {
+    res.render("users/login");
+  },
+
+
+  checkLogin: function (req, res, next) {
+    res.send("Has ingresado correctamente");
+  },
+
+  suscribe: function (req, res, next) {
+    res.render("thankYou");
   }
+  
+  
 };
 
 module.exports = usersControllers;
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,8 @@ var router = express.Router();
 let usersControllers = require('../controllers/usersControllers');
 const multer = require('multer');
 const path = require('path');
+let { check, validationResult, body} = require("express-validator");
+const fs = require("fs");
 
 let storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -14,9 +16,32 @@ let storage = multer.diskStorage({
 });
 let uploads = multer({ storage: storage });
 
+
 router.get("/", usersControllers.root);
 router.get('/register', usersControllers.register);
-router.post('/register', uploads.any(), usersControllers.registration);
+router.post('/register', uploads.any(), [
+
+  check("name").isLength({min: 2}).withMessage("* Debes completar este campo name"),
+  check("lastName").isLength({min: 3}).withMessage("* Debes completar este campo lastName"),
+  check("email").isEmail().withMessage("* Debes completar este campo email"),
+  check("password").isLength({min: 8}).withMessage("* Debes completar este campo password"),
+  check("confirmPassword").isLength({min: 8}).withMessage("* Debes completar este campo confirmPassword"),
+  body("email").custom(function (value) {
+    let usersFilePath = path.join(__dirname, '../data/users.json')
+    let usersJson = JSON.parse(fs.readFileSync(usersFilePath, {encoding: "utf-8"}));
+    let users;
+    if(usersJson == ""){
+      users = [];
+    } else {
+      users = usersJson;
+    }
+    for(let i = 0; i < users.length; i++){
+      if(users[i].email == value)
+      return false;
+    }
+  }).withMessage("email ya existente")
+
+] ,usersControllers.registration);
 router.get('/login', usersControllers.login);
 router.post('/login', usersControllers.checkLogin);
 router.post('/suscribe', usersControllers.suscribe);
