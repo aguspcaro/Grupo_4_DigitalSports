57df13354f2dc85c89e621a4efc9e5592d1a9016
aleWidomlanski <widomlanski@hotmail.com>
2021-02-26 15:53:04 -0300

    merge en products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -15,12 +15,19 @@ const userInSesion = require('../middlewares/userInSesionMiddleware');
 const userOfSesion = require('../middlewares/userOFSesionMiddleware');
 
 
+
+
+//checkeo de cada campo del formulario
 let validatorCreate = [
     check('name').isLength({min:5}).withMessage('Debe ingresar un nombre.'),
     check('deporte').isInt().withMessage('Debe seleccionar un deporte.'),
     check('marca').isInt().withMessage('Debe seleccionar una marca.'),
     check('talle').isInt().withMessage('Debe seleccionar un talle.'),
     check('publico').isLength().withMessage('Debe seleccionar el público objetivo.'),
+    check('envio').isLength().withMessage('Debe seleccionar las condiciones del envío.'),
+    check('precio').isInt().withMessage('Debe seleccionar un precio.'),
+    check('categoria').isInt().withMessage('Debe seleccionar una categoría.'),
+    check('stock').isInt().withMessage('Debe seleccionar el stock del producto.')
 ]
 
 
