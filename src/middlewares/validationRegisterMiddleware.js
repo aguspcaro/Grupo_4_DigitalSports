e5c5aa3e6ecbe1ca05206fb7e485eb494942b74f
aguspcaro <aguspcaro@gmail.com>
2021-02-28 11:48:43 -0300

    cambios

--- src/middlewares/validationRegisterMiddleware.js
+++ src/middlewares/validationRegisterMiddleware.js
@@ -2,6 +2,7 @@ let { check, body } = require('express-validator');
 const db = require('../database/models');
 const { userInfo } = require('os');
 
+
 let validationRegisterMiddleware = [
   
   check('email').isEmail().withMessage('* Debes completar este campo con un email válido'),
@@ -10,27 +11,20 @@ let validationRegisterMiddleware = [
     .isLength({ min: 8 })
     .withMessage('* La contraseña deberá ser mayor a 8 caracteres'),
 
-  /* body('email').custom(function (value) {
-    usuario = "";
-    usuario = value;
+  body('email').custom(function (value){
 
-    console.log(value);
-    db.User.findOne({ where : { email: usuario } })
-
-    .then(function(user) {
-
-      console.log(user);
-
-      if(user == null) {
-        return false
-      }
-
-      
-
-    }).catch(function(errno){console.log(errno)})
     
-   
-  }).withMessage('* Email ya existente') */
+    db.User.findOne({where: {email: value}}).then(function(busqueda){
+        
+      if( busqueda ) {
+        console.log("false");
+        return false 
+      }
+      return true
+
+    })
+    
+  }).withMessage('* Email ya existente')
 
 ];
 
