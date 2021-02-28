e2a150b0c94aee78b6f8b7e74b95b1eff53d6778
aguspcaro <aguspcaro@gmail.com>
2021-02-28 12:05:53 -0300

    errores de email, andan

--- src/middlewares/validationRegisterMiddleware.js
+++ src/middlewares/validationRegisterMiddleware.js
@@ -11,21 +11,15 @@ let validationRegisterMiddleware = [
     .isLength({ min: 8 })
     .withMessage('* La contraseña deberá ser mayor a 8 caracteres'),
 
-  body('email').custom(function (value){
+  body('email').custom(value => {
 
-    
-    db.User.findOne({where: {email: value}}).then(function(busqueda){
-        
-      if( busqueda ) {
-        console.log("false");
-        return false 
+    return db.User.findOne({where: { email: value}}).then(function(user) {
+      if (user) {
+        throw new Error(' * El email ya esta en uso');
       }
-      return true
-
     })
     
-  }).withMessage('* Email ya existente')
-
+  })
 ];
 
 module.exports = validationRegisterMiddleware;
