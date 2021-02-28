718c6df42ec15dbb0db404de1c9e49289bdb2300
aguspcaro <aguspcaro@gmail.com>
2021-02-28 12:52:18 -0300

    css products

--- src/routes/users.js
+++ src/routes/users.js
@@ -19,27 +19,27 @@ const validationEditMiddleware = require('../middlewares/validationEditMiddlewar
 const cookiesRemember = require('../middlewares/rememberMiddleware');
 
 // VISTA DEL CENTRO DE COMANDOS DEL USUARIO
-router.get('/login/perfil', userOfSesion, cookiesRemember, usersControllers.root);
+router.get('/login/perfil', userOfSesion, cookiesRemember, userInSesion, usersControllers.root);
 router.delete('/login/perfil/:id', usersControllers.delete);
 
 // VISTA DE LA EDICION DEL USUARIO
-router.get('/login/perfil/editar/:id', userOfSesion, cookiesRemember, usersControllers.mostrarUsuario);
+router.get('/login/perfil/editar/:id', userOfSesion, cookiesRemember, userInSesion, usersControllers.mostrarUsuario);
 router.put('/login/perfil/editar/:id', uploads.any(), usersControllers.editUsuario);
 
 // VISTA DE LA EDICION DEL PERFIL
-router.get('/login/perfil/editarPerfil/:id', userOfSesion, cookiesRemember, validationEditMiddleware, usersControllers.mostrarEdicionPerfil);
+router.get('/login/perfil/editarPerfil/:id', userOfSesion, cookiesRemember, userInSesion, validationEditMiddleware, usersControllers.mostrarEdicionPerfil);
 router.put('/login/perfil/editarPerfil/:id', uploads.any(), usersControllers.editPerfil);
 
 // VISTA DE LA CREACION DEL PERFIL
-router.get("/login/perfil/crearPerfil/:id", userOfSesion, cookiesRemember, usersControllers.mostrarPerfil);
+router.get("/login/perfil/crearPerfil/:id", userOfSesion, cookiesRemember, userInSesion, usersControllers.mostrarPerfil);
 router.post("/login/perfil", uploads.any(), usersControllers.createPerfil);
 
 // VISTA DEL REGISTRO DE USUARIO
-router.get('/register', userInSesion, cookiesRemember, usersControllers.mostrarRegister);
+router.get('/register', userInSesion, cookiesRemember, userInSesion, usersControllers.mostrarRegister);
 router.post('/register', uploads.any(), validationRegisterMiddleware, usersControllers.createRegister);
 
 // VISTA DEL LOGIN
-router.get('/login', cookiesRemember, userInSesion,usersControllers.login);
+router.get('/login', cookiesRemember, userInSesion, usersControllers.login);
 router.post('/login', validationLogin, usersControllers.checkLogin);
 
  //VISTA DEL USUARIO LOGUEADO
