96c5681795c1444528ec3a3583ad0c3d2c1db353
Alejandro Widomlanski <widomlanski@hotmail.com>
2021-03-04 21:05:20 -0300

    se filtra por categorias en marca y deporte

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -109,7 +109,224 @@ let mainControllers = {
     .catch(function(err) {
       console.log(err)
     })
-  }
+  },
+  sports: function(req, res) {
+
+ let busquedaRunning =  db.Products.findAll(
+  {
+    include: {association: 'sports'},
+    where: {
+      sport_id: 1
+    } 
+  },
+  )
+
+  let busquedaFutbol=  db.Products.findAll(
+    {
+      include: {association: 'sports'},
+      where: {
+        sport_id: 2
+      } 
+    },
+    )
+
+
+    let busquedaTenis =  db.Products.findAll(
+      {
+        include: {association: 'sports'},
+        where: {
+          sport_id: 3
+        } 
+      },
+      )
+    
+      let busquedaBasquet=  db.Products.findAll(
+        {
+          include: {association: 'sports'},
+          where: {
+            sport_id: 4
+          } 
+        },
+        )
+
+
+        
+    let busquedaRugby =  db.Products.findAll(
+      {
+        include: {association: 'sports'},
+        where: {
+          sport_id: 5
+        } 
+      },
+      )
+    
+      let busquedaNatacion=  db.Products.findAll(
+        {
+          include: {association: 'sports'},
+          where: {
+            sport_id: 6
+          } 
+        },
+        )
+    
+
+        let busquedaVoley =  db.Products.findAll(
+          {
+            include: {association: 'sports'},
+            where: {
+              sport_id: 7
+            } 
+          },
+          )
+        
+          let busquedaBoxeo =  db.Products.findAll(
+            {
+              include: {association: 'sports'},
+              where: {
+                sport_id: 8
+              } 
+            },
+            )
+        
+              
+          let busquedaUrbano =  db.Products.findAll(
+            {
+              include: {association: 'sports'},
+              where: {
+                sport_id: 10
+              } 
+            },
+            )
+            
+        
+     Promise.all([busquedaRunning, busquedaFutbol, busquedaTenis, busquedaBasquet, busquedaRugby, busquedaNatacion, busquedaVoley, busquedaBoxeo, busquedaUrbano])
+
+
+    .then(function(resultadoBusqueda) {
+      res.render('products/sports', {userLogueado: {}, resultadoBusqueda})
+    })
+    .catch(function(err) {
+      console.log(err)
+    })
+  },
+  marcas: function(req, res) {
+
+    let busquedaAdidas =  db.Products.findAll(
+     {
+       include: {association: 'brands'},
+       where: {
+         brand_id: 1
+       } 
+     },
+     )
+
+
+     let busquedaNike =  db.Products.findAll(
+      {
+        include: {association: 'brands'},
+        where: {
+          brand_id: 2
+        } 
+      },
+      )
+   
+    
+      let busquedaPuma =  db.Products.findAll(
+        {
+          include: {association: 'brands'},
+          where: {
+            brand_id: 3
+          } 
+        },
+        )
+   
+   
+        let busquedaLotto =  db.Products.findAll(
+         {
+           include: {association: 'brands'},
+           where: {
+             brand_id: 4
+           } 
+         },
+         )
+               
+
+         let busquedaFila =  db.Products.findAll(
+          {
+            include: {association: 'brands'},
+            where: {
+              brand_id: 5
+            } 
+          },
+          )
+     
+     
+          let busquedaHavaianas =  db.Products.findAll(
+           {
+             include: {association: 'brands'},
+             where: {
+               brand_id: 6
+             } 
+           },
+           )
+                
+
+           
+         let busquedaReebok =  db.Products.findAll(
+          {
+            include: {association: 'brands'},
+            where: {
+              brand_id: 7
+            } 
+          },
+          )
+     
+     
+          let busquedaTopper =  db.Products.findAll(
+           {
+             include: {association: 'brands'},
+             where: {
+               brand_id: 8
+             } 
+           },
+           )
+
+
+           
+          let busquedaUmbro =  db.Products.findAll(
+            {
+              include: {association: 'brands'},
+              where: {
+                brand_id: 9
+              } 
+            },
+            )
+              
+
+            let busquedaUnderArmour =  db.Products.findAll(
+              {
+                include: {association: 'brands'},
+                where: {
+                  brand_id: 10
+                } 
+              },
+              )
+                
+               
+           
+        Promise.all([busquedaAdidas, busquedaNike, busquedaPuma, busquedaLotto, busquedaFila, busquedaHavaianas, busquedaReebok, busquedaTopper, busquedaUmbro, busquedaUnderArmour])
+   
+   
+       .then(function(resultadoBusqueda) {
+         res.render('products/marcas', {userLogueado: {}, resultadoBusqueda})
+       })
+       .catch(function(err) {
+         console.log(err)
+       })
+     }
 }
+
+
+
 
 module.exports = mainControllers;
--- src/routes/main.js
+++ src/routes/main.js
@@ -9,6 +9,8 @@ router.get('/', mainControllers.index);
 router.get('/mujer', mainControllers.mujer)
 router.get('/hombre', mainControllers.hombre)
 router.get('/chicos', mainControllers.chicos)
+router.get('/sports', mainControllers.sports)
+router.get('/brands', mainControllers.marcas)
 
 /* GET Buscador Home page */
 router.get('/search', mainControllers.search);
