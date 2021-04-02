abbd28fa82da32b3a2c88339ae160fe91b8b9eff
aguspcaro <aguspcaro@gmail.com>
2021-04-02 11:00:59 -0300

    json users

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const usersControllers = require('../../controllers/api/usersControllers')
 
-router.get('/users', usersControllers.suscribe);
-router.post('/users', usersControllers.suscribe_post);
+router.get('/users/suscribe', usersControllers.suscribe);
+router.post('/users/suscribe', usersControllers.suscribe_post);
+
+router.get('/users', usersControllers.list)
 
 module.exports = router;
