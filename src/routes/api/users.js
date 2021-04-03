be49bcdf558a367ff0f90bcaac9c9eb7c70fe260
aguspcaro <aguspcaro@gmail.com>
2021-04-03 11:43:50 -0300

    route :id

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -5,4 +5,7 @@ const usersControllers = require('../../controllers/api/usersControllers')
 
 router.get('/users', usersControllers.list)
 
+router.get('/users/:id', usersControllers.detail)
+
+
 module.exports = router;
