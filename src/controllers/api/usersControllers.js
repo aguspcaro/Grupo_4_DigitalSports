638ab8129f3afaa40ef5e19047bbb85867a2d7bd
aguspcaro <aguspcaro@gmail.com>
2021-04-03 11:22:44 -0300

    api api/users

--- src/controllers/api/usersControllers.js
+++ src/controllers/api/usersControllers.js
@@ -9,17 +9,32 @@ let usersControllers =  {
     list: function(req, res) {
 
         db.User.findAll({
-            include: [{association: "profile"}]
+            include: [{association: "profile"}],
+            attributes: { exclude: ['password'] }
+
         })
             
         .then(dato => {
+            let datoNuevo =[]
+            dato.forEach(user => {
+                
+                if(user.profile == null){ 
+                    datoNuevo.push({id: user.id, email: user.email, name: null})
+                } else {
+                    datoNuevo.push({id: user.id, email: user.email, name: user.profile.first_name})
+
+                }
+            });
+
 
             let config = {
-                method: "GET",
-                headers: {
-                    "Content-Type": "aplication/json"
+                meta: {
+                    status: 200,
+                    count: dato.length,
+                    url: "/api/users"
+
                 },
-                body: dato
+                body: datoNuevo
                     
             }
 
