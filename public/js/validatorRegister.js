cbdecd434cbbbb383d6895508d803c429d7cbd9c
Alejandro Widomlanski <widomlanski@hotmail.com>
2021-04-10 15:56:44 -0300

    se agrego e.preventDefault() a login y register

--- public/js/validatorRegister.js
+++ public/js/validatorRegister.js
@@ -4,6 +4,23 @@ const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
 let email = document.querySelector("#email")
 
 
+let form = document.querySelector('.section-form-register')
+
+
+
+
+
+form.addEventListener('submit', (e) => {
+    if(email.value.length == 0 || password.value.length == 0 ) {
+        e.preventDefault()
+        document.querySelector('.erroresFrontEmail').innerHTML = "Debe completar los campos de email y password."
+    }   
+})
+
+
+
+
+
 email.addEventListener("blur", function(){
 
     if (regexEmail.test(email.value)) {
